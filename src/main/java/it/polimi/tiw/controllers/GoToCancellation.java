edddefca5230d1b423ca1b6fbc9ccc43fe8828bf
Laura <laura.colazzo@mail.polimi.it>
2022-05-13 11:37:33 +0200

    added GoToCancellation.java

---
+++ src/main/java/it/polimi/tiw/controllers/GoToCancellation.java
@@ -0,0 +1,42 @@
+package it.polimi.tiw.controllers;
+import java.io.IOException;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+
+@WebServlet("/Cancellation")
+
+public class GoToCancellation extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private TemplateEngine templateEngine;
+	
+	public void init()throws ServletException{
+		ServletContext servletContext = getServletContext();
+		ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver(servletContext);
+		templateResolver.setTemplateMode(TemplateMode.HTML);
+		this.templateEngine = new TemplateEngine();
+		this.templateEngine.setTemplateResolver(templateResolver);
+		templateResolver.setSuffix(".html");
+	}
+	
+	public void doGet(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException{
+		String path = "/WEB-INF/cancellation.html";
+		ServletContext servletContext = getServletContext();
+		final WebContext ctx = new WebContext(request, response, servletContext, request.getLocale());
+		templateEngine.process(path, ctx, response.getWriter());
+	}
+	
+	public void DoPost(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException{
+		doGet(request, response);
+	}
+}
+	
