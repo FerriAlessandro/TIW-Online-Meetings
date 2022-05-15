2885055a8c263e1ae18f1c3d59d5827ea9d6c066
FerriAlessandro <100209676+FerriAlessandro@users.noreply.github.com>
2022-05-15 18:43:06 +0200

    added filters

--- src/main/java/it/polimi/tiw/filters/LoginFilter.java
+++ src/main/java/it/polimi/tiw/filters/LoginFilter.java
@@ -13,7 +13,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-@WebFilter(servletNames = {"/CreateMeeting", "/GoToHomePage", "/SendMeetingData"})
+@WebFilter(urlPatterns = {"/CreateMeeting", "/HomePage", "/SendMeetingData"})
 
 
 public class LoginFilter implements Filter{
