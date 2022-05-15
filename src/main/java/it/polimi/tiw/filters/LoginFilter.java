544109e157608e527a6bb686346539dc12665a6d
FerriAlessandro <100209676+FerriAlessandro@users.noreply.github.com>
2022-05-15 10:48:44 +0200

    Added control of hidden parameters in CreateMeeting servlet

--- src/main/java/it/polimi/tiw/filters/LoginFilter.java
+++ src/main/java/it/polimi/tiw/filters/LoginFilter.java
@@ -13,7 +13,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-@WebFilter("/Dummy")
+@WebFilter(servletNames = {"/CreateMeeting", "/GoToHomePage", "/SendMeetingData"})
+
 
 public class LoginFilter implements Filter{
 
@@ -34,6 +35,7 @@ public class LoginFilter implements Filter{
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
 		
+		System.out.println("Checking");
 		HttpServletRequest req = (HttpServletRequest) request;
 		HttpServletResponse res = (HttpServletResponse) response;
 		String path = req.getServletContext().getContextPath() + "/index.html";
